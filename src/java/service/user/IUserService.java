d6a75d1d6c4045fe90b509dcaaab588b540c9e0a
HuyPQHE171509 <huyrongbaolon@gmail.com>
2024-12-06 13:53:43 +0700

    Update Manager Staff and Customer

--- src/java/service/user/IUserService.java
+++ src/java/service/user/IUserService.java
@@ -2,32 +2,25 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package service.user;
 
-import java.util.List;
 import model.User;
-import service.IGeneralService;
+import java.util.List;
 
-/**
- *
- * @author Admin
- */
-public interface IUserService extends IGeneralService<User>{
+public interface IUserService {
 
     // Phương thức để lấy tất cả người dùng
     List<User> findAll();
-    
+
     // Phương thức để tìm người dùng theo tên
     User getUserById(int userID);
-    
+
     // Phương thức để thêm một người dùng mới
     boolean addUser(User user);
-    
+
     // Phương thức để cập nhật thông tin người dùng
     boolean updateUser(User user);
-    
+
     // Phương thức để xóa người dùng
     boolean deleteUser(int userID);
-
 }
